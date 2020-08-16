c3794f864056c21e8e6a2fe6154c117f055ac747
Nessaj12 <rasstrelat@gmail.com>
2020-08-16 14:08:13 +0400

    first

---
+++ src/main/java/test/IndividualMethodsTest.java
@@ -0,0 +1,10 @@
+package test;
+import org.testng.annotations.*;
+public class IndividualMethodsTest
+{
+    @Test
+    public void testMethod()
+    {
+        System.out.printf("Hello!");
+    }
+}
